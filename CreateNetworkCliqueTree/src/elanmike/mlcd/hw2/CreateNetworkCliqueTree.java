d2ba28a6bc1bd92627272c83ff6e21d3509d4a38
themikesmith <themikesmith+github@gmail.com>
2013-11-10 02:31:05 -0500

    organize unused imports

--- CreateNetworkCliqueTree/src/elanmike/mlcd/hw2/CreateNetworkCliqueTree.java
+++ CreateNetworkCliqueTree/src/elanmike/mlcd/hw2/CreateNetworkCliqueTree.java
@@ -2,7 +2,6 @@ package elanmike.mlcd.hw2;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
