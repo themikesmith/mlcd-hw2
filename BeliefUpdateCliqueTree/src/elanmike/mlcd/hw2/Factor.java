1c3a04c9f0ccf67bf88019a1e37d6992876238a4
Elan <mail@elanhr.org>
2013-11-10 00:50:19 -0500

    Starting Factor

---
+++ BeliefUpdateCliqueTree/src/elanmike/mlcd/hw2/Factor.java
@@ -0,0 +1,27 @@
+package elanmike.mlcd.hw2;
+
+import java.util.ArrayList;
+
+public class Factor {
+	private static ArrayList<String> _variableNames;
+	private static ArrayList<Integer> _variableCard;
+	
+	public static 
+	
+	private int[] _variables;
+	private int _stride;
+	
+	private double[]data;
+	
+	Factor(String[] varsNames){
+		_variables=new int[varsNames.length];
+		for(int i=0; i < varsNames.length; i ++) 
+			_variables[i] = _variableNames.indexOf(varsNames[i]);
+		
+		_stride = 1;
+		for(int index:_variables) 
+			_stride*=_variableCard.get(index); 
+	}
+	
+	
+}
