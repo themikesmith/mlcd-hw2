fe5a8837b7a18b86aec9f553ae675d89aad70428
themikesmith <themikesmith+github@gmail.com>
2013-11-10 01:20:19 -0500

    initial commit! woo

---
+++ CreateNetworkCliqueTree/src/elanmike/mlcd/hw2/CreateNetworkCliqueTree.java
@@ -0,0 +1,13 @@
+package elanmike.mlcd.hw2;
+
+public class CreateNetworkCliqueTree {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
