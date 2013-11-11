5d2b5a6a571ecea8b0096d77b478a42f7b9126aa
Elan <mail@elanhr.org>
2013-11-11 00:47:01 -0500

    added new signatures for indicator function and switched to logspace

--- BeliefUpdateCliqueTree/src/elanmike/mlcd/hw2/Bump.java
+++ BeliefUpdateCliqueTree/src/elanmike/mlcd/hw2/Bump.java
@@ -127,7 +127,7 @@ public class Bump {
 			edgeToJ.getOtherVertex(this).onReceiveMessage(edgeToJ, sigmaItoJ);
 			// update edge potential
 			edgeToJ.setFactorData(sigmaItoJ);
-			if(DEBUG) System.out.println("mu I,J:\n"+edgeToJ.toStringFactor());
+			if(DEBUG) System.out.println("mu I,J:\n"+(Factor)edgeToJ);
 		}
 		/**
 		 * When we receive a message...
@@ -136,13 +136,10 @@ public class Bump {
 		 * @param sigmaItoJ
 		 */
 		private void onReceiveMessage(Edge edgeItoJ, Factor sigmaItoJ) throws FactorException {
-			if(DEBUG) {
-				System.out.println(this.toString()+" receiving msg from:"+edgeItoJ.getOtherVertex(this));
-				System.out.println("mu I,J:"+edgeItoJ.toStringFactor());
-			}
+			if(DEBUG) System.out.println(this.toString()+" receiving msg from:"+edgeItoJ.getOtherVertex(this));
 			// belief j = belief j * (sigma ij / mu ij)
 			this.setFactorData(this.product(sigmaItoJ.divide(edgeItoJ)));
-			if(DEBUG) System.out.println("belief J:\n"+super.toString());
+			if(DEBUG) System.out.println("belief J:\n"+(Factor)this);
 			// check if I was informed when sending
 			Vertex i = edgeItoJ.getOtherVertex(this);
 			_recvdMsgStatus.put(edgeItoJ, i._isInformed);
@@ -237,9 +234,6 @@ public class Bump {
 			sb.append(EDGE);
 			sb.append(Factor.variableIndicesToNames(_two._variables));
 			return sb.toString();
-		}
-		public String toStringFactor() {
-			return super.toString();
 		}
 		public String getLongInfo() {
 			StringBuilder sb = new StringBuilder("edge:\n");
@@ -436,7 +430,7 @@ public class Bump {
 				}
 			}
 			Vertex curr = toProcess.remove();
-			if(DEBUG) System.out.printf("\ncurr vertex:%s\n",curr);
+			if(DEBUG) System.out.printf("curr vertex:%s\n",curr);
 			// mark
 			curr.setOrderID();
 			ordering.add(curr); // and add to our ordered list
@@ -467,7 +461,6 @@ public class Bump {
 		if(DEBUG) System.out.println("\n\nupward pass!\n\n");
 		for(int i = orderedVertices.size() - 1; i >= 0; i--) {
 			Vertex v = orderedVertices.get(i);
-			if(DEBUG) System.out.printf("\ncurr vertex:%s\n",v);
 			// for each edge that is outgoing given our ordering
 			for(Edge e : v.getUpwardOutgoingNeighborEdges()) {
 				// send our message along that edge
